ee0fe80c243769c2aa7fdaa9e0105be7256afe2b
Mehar Singh <msing043@wch136-24.cs.ucr.edu>
2021-04-10 21:33:36 -0700

    Committing second excersize

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -13,11 +13,31 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
+DDRA = 0x00; PORTA = 0xFF; 
+DDRB = 0xFF; PORTB = 0x00;
+unsigned char tmpA = 0x00;
+unsigned char cntavail=0x00;
+ while (1) {
+	cntavail=PORTC;
+	tmpA= PINA;
+	if(tmpA==0x00){
+	cntavail=0x04;
+	}
+	else if((tmpA==0x01)|| (tmpA==0x02) || (tmpA==0x04) || (tmpA==0x08)){
+	cntavail=0x03;
+	}
+	else if((tmpA==0x03) || (tmpA==0x05) || (tmpA==0x06) || (tmpA==0x09) || (tmpA==0xA0)||(tmpA==0xC0)){
+	cntavail=0x02;
+	}
+	else if((tmpA==0x07)||(tmpA==0x0E)||(tmpA==0x0D)||(tmpA==0x0B)){
+	cntavail=0x01;
+	}
+	else{
+	cntavail=0x00;
 }
+	PORTC=cntavail;
+}
+return 1;
+}
+
+
