a42035daf0e44ac9c15ac76a810e0673e0b59307
Mehar Singh <msing043@wch136-24.cs.ucr.edu>
2021-04-10 22:05:46 -0700

    Committing all changes

--- Lab2_introToAVR/turnin/[cslogin]_lab2_part2.c
+++ Lab2_introToAVR/turnin/[cslogin]_lab2_part2.c
@@ -16,7 +16,7 @@ int main(void) {
 DDRA = 0x00; PORTA = 0xFF; 
 DDRB = 0xFF; PORTB = 0x00;
 unsigned char tmpA = 0x00;
-unsigned char cntavail=0x00;
+unsigned char cntavail;
  while (1) {
 	cntavail=PORTC;
 	tmpA= PINA;
@@ -26,7 +26,7 @@ unsigned char cntavail=0x00;
 	else if((tmpA==0x01)|| (tmpA==0x02) || (tmpA==0x04) || (tmpA==0x08)){
 	cntavail=0x03;
 	}
-	else if((tmpA==0x03) || (tmpA==0x05) || (tmpA==0x06) || (tmpA==0x09) || (tmpA==0xA0)||(tmpA==0xC0)){
+	else if((tmpA==0x03) || (tmpA==0x05) || (tmpA==0x06) || (tmpA==0x09) || (tmpA==0x0A)||(tmpA==0x0C)){
 	cntavail=0x02;
 	}
 	else if((tmpA==0x07)||(tmpA==0x0E)||(tmpA==0x0D)||(tmpA==0x0B)){
